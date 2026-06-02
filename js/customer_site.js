585713949f208dfa9a5bc9dae1500542bf6d516f
mclei <2636827967@qq.com>
2026-06-02 16:36:22 +0800

    Fix formatting in CUSTOMER_SITES object

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,10 +1,11 @@
 const CUSTOMER_SITES = {
-      iqiyizyapi: {
-        name: '🎬-爱奇艺-',
-        api: 'https://iqiyizyapi.com/api.php/provide/vod',
-        detail: 'https://iqiyizyapi.com',
-    }
+   iqiyizyapi: {
+       name: '🎬-爱奇艺-',
+       api: 'https://iqiyizyapi.com/api.php/provide/vod',
+       detail: 'https://iqiyizyapi.com',
+      }
 };
+
 // 调用全局方法合并
 if (window.extendAPISites) {
     window.extendAPISites(CUSTOMER_SITES);
